9f0bfaf25a48a3f470b6b162b1caf61c21d116e8
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-28 08:13:59 +0530

    Create coin.py

---
+++ coin.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] += dp[i-coin]
+        return dp[amount]
+val=Solution()
+n,amount=map(int,input().split())
+coins=list(map(int,input().split()))
+print(val.change(amount,coins))
